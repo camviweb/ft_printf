1485a8046172ea51dbd0080bc7a1294a4caea5ef
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-03-08 16:43:53 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -11,18 +11,18 @@
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# include <stdarg.h> // va_start, va_arg, va_copy, va_end
-# include <stdlib.h> // malloc et free
-# include <unistd.h> // write
+# include <stdarg.h> 
+# include <stdlib.h> 
+# include <unistd.h>
 
 int	ft_printf(const char *s, ...);
-int	ft_strlen(char *str);
 int	ft_putchar(char c);
 int	ft_putptr(unsigned long long ptr);
 int	ft_putstr(char *s);
 int	ft_putnbr(int n);
 int	ft_putunbr(unsigned int n);
-int	ft_putlx(unsigned int n);
-int	ft_putux(unsigned int n);
+int	ft_putx(unsigned int n, char c);
+int	do_format(const char *s, va_list args, int i);
+int	handle_pointer_null(unsigned long long adr);
 
 #endif
