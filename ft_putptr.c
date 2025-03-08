87b44706054530df7d2a3743f75844a6d4b06f34
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-03-08 16:40:24 +0100

    Update ft_putptr.c

--- ft_putptr.c
+++ ft_putptr.c
@@ -1,13 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 14:26:59 by canguyen          #+#    #+#             */
+/*   Updated: 2025/03/08 14:27:03 by canguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int	ft_putptr(unsigned long long adr)
 {
-	char *hexa;
-	int len;
-	int tmp;
+	char	*hexa;
+	int		len;
+	int		tmp;
 
 	len = 0;
 	hexa = "0123456789abcdef";
+	if (adr == 0)
+	{
+		return (ft_putstr("(nil)"));
+	}
 	if (adr >= 16)
 	{
 		tmp = ft_putptr(adr / 16);
